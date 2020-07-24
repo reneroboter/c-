249ec921bf26f93f5a6c7b90d0544242c8f2f31d
=René Roboter <rene.roboter@posteo.de>
2020-07-24 20:55:26 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApplication1/Calculator.cpp
@@ -0,0 +1,18 @@
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char operand, double y)
+{
+    switch (operand)
+    {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+        default:
+            return 0.0;
+    }
+}
---
+++ enc_temp_folder/f2c51c5f9ceecc94bca3ec7f6b398e15/ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+// ConsoleApplication1.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char operand = '+';
+
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform, Format: a+b | a-b | a*b | a/b"
+        << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> operand >> y;
+
+        if (x && y <= 0.0) {
+            exit(1);
+        }
+        result = c.Calculate(x, operand, y);
+        cout << "Result is: " << result << endl;
+    }
+
+    return 0;
+}
+
+// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
